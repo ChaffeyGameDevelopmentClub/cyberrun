a9703fc23fe6bfd69f96fd680f6c89e230b385ac
The Bron Jame Offical <91028605+TheBronJameOffical@users.noreply.github.com>
2024-11-22 01:49:36 -0800

    stuf

--- Godot/cyberrun/Scripts/Player/player.gd
+++ Godot/cyberrun/Scripts/Player/player.gd
@@ -1,27 +1,19 @@
 class_name Player extends CharacterBody3D
-#pause my menu idiot
-@export var PauseMenu: Control
-var paused = false
 
 #dont tell them this but i stole everything from backwaters
 #also dont tell them but im using this as my checklist hahahahahahahahaha
 #TODO:
-#Sliding, set distance at higher velocity, done from sprinting speed
-#Wall Jump, detect if on wall when moving, apply velocity opposite of direction
 #Dashing, kinda like a teleport, ask luna later lmao
 #General UI
-#oh wow theres a lot
-#help me
 #add in acceleration a bit, start at lower velocity, timer goes off and velocity ups
 #going into idle resets timer
 
-#SFX
-#@export var Walk: AudioStreamPlayer
-#@export var Run: AudioStreamPlayer
 #Jumping :3
 const jump_velocity = 4.5
 var max_double_jump : int = 2
 var double_jumps : int = 2
+var wall_jumps : int = 3
+var max_wall_jumps : int = 3
 #Toggles for your actions, just so that its not a pain in the ass later
 @export var canDJump = true
 @export var canDash = true
@@ -31,14 +23,15 @@ var double_jumps : int = 2
 @export var player_speed = 6
 @export var head : Node3D
 @export var camera : Camera3D
-#gorl what does this mean
-@export var isSound = false
+#Walljump
+@export var wallJumpRay = RayCast3D
 #Headbob, it affects where you aim so like.... maybe not too much unless im goated and figure out how to seperate the two
-const headbob_move_amt = 0.06
+const headbob_move_amt = 0.04
 const headbob_freq = 2.4
 var headbob_time := 0.0
 var newV
 
+#State array
 enum PLAYER_STATE{
 	Idle,
 	Air,
@@ -49,6 +42,7 @@ enum PLAYER_STATE{
 
 #pause menu or something idk
 @export var pause : Control
+var paused = false
 
 func _ready() -> void:
 	Global.player = self
@@ -81,14 +75,16 @@ func _physics_process(delta: float) -> void:
 	Global.debug.add_property("VelocityY",velocity.y,4)
 	Global.debug.add_property("CurrentState",player_state, 5)
 	Global.debug.add_property("DoubleJumps",double_jumps, 6)
+	Global.debug.add_property("WallJumps",wall_jumps, 7)
 	
-	_headbob_effect(delta)
 	if is_on_floor():
 		double_jumps = max_double_jump
+		wall_jumps = max_wall_jumps
 	if not is_on_floor():
 		player_state = 1
-		
-	match player_state: #Player states, this looks so dirty im kaying my essing self
+	
+	#Condense the update functions
+	match player_state: #Player states
 		0: #Idle
 			update_gravity(delta)
 			update_input(player_speed)
@@ -101,13 +97,19 @@ func _physics_process(delta: float) -> void:
 			update_velocity()
 			if is_on_floor():
 				player_state = 0
+			#Buggy, figure out how to make it not conflict with wall jumping
+			#if Input.is_action_pressed("left") and is_on_wall() and !Input.is_action_just_pressed("jump"):
+				#velocity.y = -1
+			#if Input.is_action_pressed("right") and is_on_wall() and !Input.is_action_just_pressed("jump"):
+				#velocity.y = -1
 		2: #Walking
 			update_gravity(delta)
 			update_input(player_speed)
 			update_velocity()
+			_headbob_effect(delta)
 			if velocity == Vector3(0,0,0):
 				player_state = 0
-			if Input.is_action_just_pressed("slide"):
+			if Input.is_action_just_pressed("slide") and canSlide == true:
 				player_state = 3
 				newV = velocity * 1.5
 				
@@ -129,6 +131,7 @@ func update_input(player_speed: float) -> void:
 	if direction:
 		velocity.x = direction.x * player_speed
 		velocity.z = direction.z * player_speed
+		wallJumpRay.target_position = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, player_speed)
 		velocity.z = move_toward(velocity.z, 0, player_speed)
@@ -141,18 +144,30 @@ func _input(event: InputEvent) -> void:
 	# Jumping or something
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
-	if Input.is_action_just_pressed("jump") and not is_on_floor() and canDJump:
-		if double_jumps <= 0:
-			pass
-		#if is_on_wall():
-		
-		#else:
-			velocity.y = jump_velocity
-			double_jumps -= 1
+	if Input.is_action_just_pressed("jump") and not is_on_floor():
+		if is_on_wall():
+			if wall_jumps <= 0:
+				pass
+			else:
+				#Add in the bounceback
+				velocity.y = jump_velocity
+				velocity.x = checkRayCast().x*2
+				velocity.z = checkRayCast().z*2
+				wall_jumps -= 1
+		elif canDJump:
+			if double_jumps <= 0:
+				pass
+			else:
+				velocity.y = jump_velocity
+				double_jumps -= 1
 	#Do later
 	if Input.is_action_just_pressed("pause"):
 		get_tree().quit()
 		#Pause()
+
+func checkRayCast():
+	var direction = wallJumpRay.target_position * - 1
+	return direction
 
 #Pause, completely pauses everything so you can go into options
 func Pause():
