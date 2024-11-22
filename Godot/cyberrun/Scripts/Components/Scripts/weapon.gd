a8ef4514a9642e4a19322c66613cc9f042711f3e
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-11-21 20:03:45 -0800

    idk

--- Godot/cyberrun/Scripts/Components/Scripts/weapon.gd
+++ Godot/cyberrun/Scripts/Components/Scripts/weapon.gd
@@ -3,3 +3,8 @@ extends Node3D
 var attack_damage := 10.0
 var knockback_force := 100.0
 var stun_time := 1.5
+
+
+func _on_hitbox_component_area_entered(area: Area3D) -> void:
+	if area is HitboxComponent:
+		var hitbox : HitboxComponent = area
